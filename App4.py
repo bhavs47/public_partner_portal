5169981123c9513d3de1509a8e17759f8ec99d00
bhavs47 <bhavya.nair@nihr.ac.uk>
2025-11-27 16:41:29 +0000

    Update App4.py

--- App4.py
+++ App4.py
@@ -330,13 +330,13 @@ results = filter_dataframe(df, filters)
 
 
 # --- Display table ---
-display_col = results  # show all columns
+display_df = results  # show all columns
 #display_cols = [name_col, email_col]
 #for c in disease_cols + [age_col, gender_col, carer_col, ethnicity_col, expertise_col]:
     #if c and c not in display_cols:
         #display_cols.append(c)
 
-display_df = results[display_cols]
+#display_df = results[display_cols]
 
 st.write("---")
 res1, res2 = st.columns([1,3])
@@ -366,6 +366,7 @@ st.markdown(
     "Tips: Upload an Excel (.xlsx) or CSV containing Name, Email, and Disease columns. "
     "You can map your own columns above."
 )
+
 
 
 
