d6c4525191b226bbd1a1a50c02031cebbce56426
bhavs47 <bhavya.nair@nihr.ac.uk>
2025-12-02 14:35:22 +0000

    Update App4.py

--- App4.py
+++ App4.py
@@ -230,7 +230,8 @@ with st.container():
 st.write("---")
 
 # Raw GitHub URL (make sure it ends with .xlsx)
-dataset_url = "https://raw.githubusercontent.com/bhavs47/public_partner_portal/main/Database.xlsx"
+dataset_url = "https://github.com/bhavs47/public_partner_portal/blob/main/Database.xlsx"
+#dataset_url = "https://raw.githubusercontent.com/bhavs47/public_partner_portal/main/Database.xlsx" 
 
 try:
     # Download the file content
@@ -503,6 +504,7 @@ st.markdown(
     "Tips: Upload an Excel (.xlsx) or CSV containing Name, Email, and Disease columns. "
     "You can map your own columns above."
 )
+
 
 
 
