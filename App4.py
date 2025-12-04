dc7b5ffbf9f2d685cc18a87f95454ba33fcd196b
bhavs47 <bhavya.nair@nihr.ac.uk>
2025-12-04 13:23:16 +0000

    Update App4.py

--- App4.py
+++ App4.py
@@ -88,9 +88,6 @@ if "access_token" not in token_result:
 email = token_result["id_token_claims"].get("preferred_username")
 st.session_state["user_email"] = email
 
-st.write("email?", email)
-st.write("allowed_emails?", ALLOWED_EMAILS)
-
 if email not in ALLOWED_EMAILS:
     st.error("❌ You do not have permission to access this tool.")
     st.stop()
@@ -510,6 +507,7 @@ st.markdown(
     "Tips: Upload an Excel (.xlsx) or CSV containing Name, Email, and Disease columns. "
     "You can map your own columns above."
 )
+
 
 
 
