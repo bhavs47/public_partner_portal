be1e1dfe7e0d3f5ac09640e78755d95e3491ccc6
bhavs47 <bhavya.nair@nihr.ac.uk>
2025-12-04 14:24:02 +0000

    Update App4.py

--- App4.py
+++ App4.py
@@ -99,7 +99,7 @@ if email not in ALLOWED_EMAILS:
     st.error("❌ You do not have permission to access this tool.")
     st.stop()
 
-st.success(f"✅ Signed in as {email}")
+#st.success(f"✅ Signed in as {email}")
 
 
 
@@ -543,6 +543,7 @@ st.markdown(
     "Tips: Upload an Excel (.xlsx) or CSV containing Name, Email, and Disease columns. "
     "You can map your own columns above."
 )
+
 
 
 
